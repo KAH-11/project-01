833f210e17c4cace5b33e297982fbf2b27a9c8c8
KAH-11 <135812052+KAH-11@users.noreply.github.com>
2023-06-21 14:37:35 +0300

    create functions in category controller

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,8 +3,72 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Category;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class CategoryController extends Controller
 {
-    //
+    function index(Request $request)
+    {
+        $categories = QueryBuilder::for(Category::class)
+        ->allowedFilters(['name'])
+        ->allowedSorts(['name','id'])
+        ->paginate();
+
+        $response = [
+            'categories' => $categories
+        ];
+        return response($response);
+    }
+
+    function store(Request $request)
+    {
+        $validated=$request->validate([
+            'name' => 'required|unique',
+        ]);
+
+        $new_category= Category::create([
+            'name' => $request->name,
+            'is_active' => True,
+        ]);
+
+        $response = [
+            'message' => ['New Category Created Successfully!']
+        ];
+        return response($response);
+        
+    }
+
+    function show(Category $category)
+    {
+        $response = [
+            'Category' => $category
+        ];
+        return response($response);
+    }
+
+    function update(Category $category,Request $request)
+    {
+
+        $validated=$request->validate([
+            'name' => 'required|unique',
+        ]);
+
+        $category->name = $request->name;
+        $category->is_active = $request->is_active;
+        $category->save();
+
+        $response = [
+            'category' => $category
+        ];
+        return response($response);
+    }
+
+    function destroy(Category $category)
+    { 
+        $category->delete();
+        return response('Category Deleted!');
+    }
 }
