594733a049bbed8e906cbbdf5726d904e3b9923e
KAH-11 <135812052+KAH-11@users.noreply.github.com>
2023-06-22 14:05:41 +0300

    Edit migration

--- database/migrations/2023_06_21_105752_create_categories_table.php
+++ database/migrations/2023_06_21_105752_create_categories_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id('id');
             $table->string('name')->unique();
-            $table->boolean('is_active');
+            $table->boolean('is_active')->default(true);
             $table->timestamps();
         });
     }
