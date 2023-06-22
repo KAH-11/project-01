989808d876670799389a21b4de741fe6d014042c
KAH-11 <135812052+KAH-11@users.noreply.github.com>
2023-06-22 13:58:54 +0300

    Edit Controller

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -31,8 +31,8 @@ class CategoryController extends Controller
         ]);
 
         $new_category= Category::create([
-            'name' => $request->name
-            // 'is_active' => True,
+            'name' => $request->name,
+            'is_active' => True,
         ]);
 
         $response = [
