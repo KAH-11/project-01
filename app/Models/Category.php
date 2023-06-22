edfb97d315a50f2a7ed9792729b7ad90265ec719
KAH-11 <135812052+KAH-11@users.noreply.github.com>
2023-06-22 12:55:52 +0300

    Edit relations between products and categories

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,15 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Category extends Model
 {
     use HasFactory;
 
-    protected $fillable = [
+    protected $guarded = [
         'id',
         'name',
         'is_active'
     ];
+
+    public function products():HasMany{
+        return $this->hasMany(Product::class);
+    }
 
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,12 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Product extends Model
 {
     use HasFactory;
 
-    protected $fillable = [
+    protected $guarded = [
         'id',
         'name',
         'price',
@@ -17,5 +18,9 @@ class Product extends Model
         'description',
         'cat_id'
     ];
+
+    public function category():BelongsTo{
+        return $this->belongsTo(Category::class);
+    }
 
 }
